ba3267f58ab67a6b58a7dc21f50197aaae03522f
Tim Petrvalsky <tpet55@yahoo.com>
2022-01-07 10:40:18 -0700

    Initial commit

---
+++ server/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+const app = express();
+app.use(express.json());
+import cors from 'cors';
+
+app.use(cors());
+
+const PORT = 3000;
+
+app.get('/api/ping', (_req, res) => {
+  console.log('someone pinged here');
+  res.send('pong');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
